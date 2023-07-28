3df033a491247f15cee5d844d5b94e583452610f
cedric <cedricj765@gmail.com>
2023-07-28 12:11:56 -0500

    complated lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,15 @@
 import React from "react";
+import * as User from '../data/user'
+
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <img src={User.image} alt="I made this" />
     </div>
   );
 }
+
+export default About
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
+import * as User from '../data/user'
 
 function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        {User.username} is a Web Developer from {User.city}
       </h1>
     </div>
   );
 }
+
+export default Home
